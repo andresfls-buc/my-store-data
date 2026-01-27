fc4810a58a6e99158fe074658226553a58296adc
Andres Landazabal <andresflsxx@gmail.com>
2026-01-28 08:43:42 +0900

    add: sign with JWT in the login using the payload and generating a token

--- config/config.js
+++ config/config.js
@@ -10,6 +10,7 @@ const config = {
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
   apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET || 'myCat',
   
   /* FIX: Ensure this matches the EXACT name you used in the Vercel Dashboard.
      Vercel usually provides 'DATABASE_URL' automatically if using their Postgres,
